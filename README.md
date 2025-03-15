23c6256b1d1a9c3cb4b5a8576e5822010b62a865
Mohammad Zgoul <moeazgoul@gmail.com>
2025-03-16 00:38:50 +0300

    hero section done

---
+++ README.md
@@ -0,0 +1,5 @@
+# Saas Landing Page (XORA)
+
+This project uses the following [design](https://ui8.net/ui8/products/xora---saas-landing-page-kit?rel=jsm)
+
+built using react and tailwindcss.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./sections/header";
+import Hero from "./sections/Hero";
+
+const App = () => {
+  return <main className="overflow-hidden">
+    <Header />
+    <Hero />
+  </main>;
+};
+
+export default App;
