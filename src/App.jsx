5b073c5fc499bda4c2d24a660efddf44281a7fd1
Mohammad Zgoul <moeazgoul@gmail.com>
2025-03-22 01:48:03 +0300

    added faq sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./sections/header";
 import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
+import Faq from "./sections/Faq";
 
 const App = () => {
   return <main className="overflow-hidden">
@@ -10,6 +11,7 @@ const App = () => {
     <Hero />
     <Features />
     <Pricing />
+    <Faq />
   </main>;
 };
 
