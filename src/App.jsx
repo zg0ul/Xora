f9f923e1ec5ad14e17b1daa80e4aecab2b73a19a
Mohammad Zgoul <moeazgoul@gmail.com>
2025-03-22 20:14:24 +0300

    added testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
 import Faq from "./sections/Faq";
+import Testimonials from "./sections/Testimonials";
 
 const App = () => {
   return <main className="overflow-hidden">
@@ -12,6 +13,7 @@ const App = () => {
     <Features />
     <Pricing />
     <Faq />
+    <Testimonials />
   </main>;
 };
 
