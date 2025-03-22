9d01f484eaa89bd713959b5f5b02294b1b5545da
Mohammad Zgoul <moeazgoul@gmail.com>
2025-03-23 00:51:45 +0300

    added Download and Footer sections

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
 import Faq from "./sections/Faq";
 import Testimonials from "./sections/Testimonials";
+import Download from "./sections/Download";
+import Footer from "./sections/Footer";
 
 const App = () => {
   return <main className="overflow-hidden">
@@ -14,6 +16,8 @@ const App = () => {
     <Pricing />
     <Faq />
     <Testimonials />
+    <Download />
+    <Footer />
   </main>;
 };
 
